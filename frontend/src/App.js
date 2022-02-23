cd7f146e254c2640ca1fa0cb7388f28853875b56
TausifM <nisargcontact2020@gmail.com>
2022-02-22 23:23:41 -1100

    Hero Section Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { Navbar } from "./components";
-
+import {Header} from './container'
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Header />
+    </>
   );
 }
 
---
+++ frontend/src/container/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header";
+import Skills from './Skills/Skills';
+import About from './About/About';
+import Work from './Work/Work';
+import Testimonials from './Testimonials/Testimonials';
+import Footer from './Footer/Footer';
+
+
+export {
+    Header,
+    Skills,
+    About,
+    Work,
+    Testimonials,
+    Footer
+}
