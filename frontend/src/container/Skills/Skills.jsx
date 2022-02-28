5f2643553956baadeb381e2f4f4bd216a91e8f86
TausifM <nisargcontact2020@gmail.com>
2022-02-28 16:53:44 +0545

    Skills all set

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -6,7 +6,7 @@ import ReactTooltip from "react-tooltip";
 import "./Skills.scss";
 const Skills = () => {
   const [skills, setSkills] = useState([]);
-  const [experience, setExperience] = useState([]);
+  const [experiences, setExperience] = useState([]);
   useEffect(() => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
@@ -20,8 +20,9 @@ const Skills = () => {
   return (
     <>
       <h2 className="head-text">
-        Skills & <span>Experience</span>
+        Skills & <span>Experiences</span>
       </h2>
+
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
           {skills.map((skill) => (
@@ -41,9 +42,43 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
 };
 
-export default Skills;
+export default AppWrap(Skills, 'skills');
